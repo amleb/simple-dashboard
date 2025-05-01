88184293d5ae188f8b3a5927b72511d376925314
Adam Lebioda <adam.m.lebioda@gmail.com>
2025-05-01 12:41:35 +0200

    add theme switcher

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,50 +1,58 @@
 import { Refine } from "@refinedev/core";
-import {
-    ErrorComponent,
-} from "@refinedev/antd";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ErrorComponent, ThemedLayoutV2, useNotificationProvider, } from "@refinedev/antd";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import WelcomePage from "./pages/WelcomePage";
 import S3BucketsPage from "./pages/S3BucketsPage";
 import SQSQueuesPage from "./pages/SQSQueuesPage";
 import { RegionProvider } from "./contexts/RegionContext";
-import { CustomLayout } from "./components/CustomLayout";
-
 import { client } from './lib/apollo';
 import React from 'react';
+import { TopBar } from './components/TopBar';
+import { DocumentTitleHandler, UnsavedChangesNotifier } from '@refinedev/react-router';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 const App: React.FC = () => {
     return (<ApolloProvider client={client}>
-        <RegionProvider>
-            <BrowserRouter>
-                <Refine
-                    resources={[{
-                        name: "buckets", list: "/buckets", meta: {label: "S3 Buckets"},
-                    }, {
-                        name: "queues", list: "/queues", meta: {label: "SQS Queues"},
-                    },]}
-                >
-                    <CustomLayout>
-                        <Routes>
-                            <Route
-                                path="/"
-                                element={<WelcomePage/>}
-                            />
-                            <Route
-                                path="buckets"
-                                element={<S3BucketsPage/>}
-                            />
-                            <Route
-                                path="queues"
-                                element={<SQSQueuesPage/>}
-                            />
-                            <Route path="*" element={<ErrorComponent/>}/>
-                        </Routes>
-                    </CustomLayout>
-                </Refine>
-            </BrowserRouter>
-        </RegionProvider>
+        <ThemeProvider>
+            <RegionProvider>
+                <BrowserRouter>
+                    <Refine
+                        resources={[{
+                            name: "buckets", list: "/buckets", meta: {label: "S3 Buckets"},
+                        }, {
+                            name: "queues", list: "/queues", meta: {label: "SQS Queues"},
+                        },]}
+                        notificationProvider={useNotificationProvider}
+                        options={{
+                            syncWithLocation: true, warnWhenUnsavedChanges: true,
+                        }}
+                    >
+                        <ThemedLayoutV2
+                            Header={() => <TopBar/>}
+                        >
+                            <Routes>
+                                <Route
+                                    path="/"
+                                    element={<WelcomePage/>}
+                                />
+                                <Route
+                                    path="buckets"
+                                    element={<S3BucketsPage/>}
+                                />
+                                <Route
+                                    path="queues"
+                                    element={<SQSQueuesPage/>}
+                                />
+                                <Route path="*" element={<ErrorComponent/>}/>
+                            </Routes>
+                            <UnsavedChangesNotifier/>
+                            <DocumentTitleHandler/>
+                        </ThemedLayoutV2>
+                    </Refine>
+                </BrowserRouter>
+            </RegionProvider>
+        </ThemeProvider>
     </ApolloProvider>);
 };
 
--- apps/frontend/src/components/TopBar.tsx
+++ apps/frontend/src/components/TopBar.tsx
@@ -1,29 +1,75 @@
 import React from "react";
-import { Layout, Select } from "antd";
+import { Button, Layout, Select } from "antd";
 import { useRegion } from "../contexts/RegionContext";
+import { useTheme } from "../contexts/ThemeContext";
 
-const { Header } = Layout;
+const {Header} = Layout;
 
-const regions = [
-    "us-east-1",
-    "us-west-1",
-    "us-west-2",
-    "eu-central-1",
-    "eu-west-1",
-];
+const regions = ["us-east-1", "us-west-1", "us-west-2", "eu-central-1", "eu-west-1",];
 
-export const TopBar: React.FC = () => {
-    const { region, setRegion } = useRegion();
+interface HeaderProps {
+    theme: "light" | "dark";
+    setTheme: (theme: "light" | "dark") => void;
+}
 
-    return (
-        <Header style={{ background: "#fff", padding: "0 16px", display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
-            <span style={{ marginRight: 8 }}>Region:</span>
+const IconSun = () => (<svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-sun"
+        width={18}
+        height={18}
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        stroke="currentColor"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <circle cx={12} cy={12} r={4}/>
+        <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"/>
+    </svg>);
+
+const IconMoonStars = () => (<svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-moon-stars"
+        width={18}
+        height={18}
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"/>
+        <path d="M17 4a2 2 0 0 0 2 2a2 2 0 0 0 -2 2a2 2 0 0 0 -2 -2a2 2 0 0 0 2 -2"/>
+        <path d="M19 11h2m-1 -1v2"/>
+    </svg>);
+
+export const TopBar: React.FC = (props) => {
+    const {region, setRegion} = useRegion();
+    const {theme, toggleTheme} = useTheme();
+
+    const color = theme === "light" ? "#fff" : "#141414";
+
+    return (<Header style={{
+            background: color,
+            padding: "0 16px",
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center"
+        }}>
+            <span style={{marginRight: 8}}>Region:</span>
             <Select
-                style={{ width: 160 }}
+                style={{width: 160}}
                 value={region}
                 onChange={setRegion}
-                options={regions.map((r) => ({ value: r, label: r }))}
+                options={regions.map((r) => ({value: r, label: r}))}
             />
-        </Header>
-    );
+            <Button
+                onClick={toggleTheme}
+                icon={theme === "light" ? <IconMoonStars/> : <IconSun/>}
+            />
+        </Header>);
 };
